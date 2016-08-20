8fb24c6f20cde23f9925d955fdc67ba267a61287
Poren Chiang <ren.chiang@gmail.com>
2016-08-20 14:05:19 +0800

    Ball shoot is working!!!

--- js/objects/ball.js
+++ js/objects/ball.js
@@ -1,13 +1,28 @@
-define(['threejs', 'cannon', 'scene', 'loaders', 'physics'], function(THREE, CANNON, scene, loaders, physics) {
+define(['threejs', 'cannon', 'scene', 'camera', 'renderer', 'loaders', 'physics'],
+function(THREE, CANNON, scene, camera, renderer, loaders, physics) {
     var shape = new CANNON.Sphere(150);
     var material = new CANNON.Material();
     physics.world.addContactMaterial(new CANNON.ContactMaterial(physics.ground.material, material, { friction: 0.2, restitution: 0.2 }));
 
     var body = new CANNON.Body({ mass: 1, shape: shape, material: material });
-    body.velocity.set(1300, 9200, 0);
+    //body.velocity.set(1300, 9200, 0);
+
+    var z = new THREE.Vector3(0, 0, 1);
+    var velocity = 9300;
+    var direction = new THREE.Vector3();
+
+    var canvas = renderer.domElement;
+    function shootBall(e) {
+        body.position.set(0, 0, 0);
+
+        direction.set(0, 0, 1).unproject(camera).normalize();
+        body.velocity.set(direction.x * velocity, direction.y * velocity, direction.z * velocity);
+    }
+
+    canvas.addEventListener('mouseup', shootBall);
+    canvas.addEventListener('touchend', shootBall);
 
     loaders.loadTexturedOBJ('ball').then(function(ball) {
-        //ball.position.set(0, -500, 0);
         physics.bind(ball, body);
         scene.add(ball);
     });
