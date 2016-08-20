1d08dd19929722fbaff4fdc61a9728b5722241fc
Poren Chiang <ren.chiang@gmail.com>
2016-08-20 11:40:13 +0800

    Add binding functions to physics

--- js/physics.js
+++ js/physics.js
@@ -8,10 +8,28 @@ define(['cannon'], function(CANNON) {
     world.broadphase = new CANNON.NaiveBroadphase();
     world.solver.iterations = 10;
 
+    var ground = new CANNON.Body({ mass: 0 });
+    ground.addShape(new CANNON.Plane());
+    ground.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2); // Rotate from Z-up to Y-up
+    world.add(ground);
+
+    // Pair of three.js - cannon.js objects to keep track of
+    var bodies = [];
+
     return {
         world: world,
+        bind: function(obj, body) {
+            body.position.copy(obj.position);
+            body.quaternion.copy(obj.quaternion);
+            bodies.push({ object: obj, body: body });
+            world.add(body);
+        },
         update: function() {
             world.step(timestep);
+            for (var i in bodies) {
+                bodies[i].object.position.copy(bodies[i].body.position);
+                bodies[i].object.quaternion.copy(bodies[i].body.quaternion);
+            }
         }
     }
 });
